06f24db90f477f07914c9cd91928668747e5afcc
Philipp Benner <philipp.benner@mis.mpg.de>
2013-01-14 13:41:21 +0100

    2013/01/14-13:41:21 (Linux ibmterm01 unknown)

--- gp.R
+++ gp.R
@@ -376,3 +376,24 @@ gt <- new.gt(x,
 
 sample.with.gt(e, x, gt)
 plot(e, 1:100/20)
+
+# Video
+################################################################################
+
+x        <- 0:100/20
+theta    <- (1-tanh(-50:50/20))/2.5+0.1
+
+e        <- new.experiment(c(2.0,2.0),
+                           partially.apply(kernel.exponential, 0.8))
+sample.x <- 0:10/2
+gt       <- new.gt(x, theta)
+
+for (i in 1:250) {
+  sample.with.gt(e, sample.x, gt)
+  png(filename=sprintf("plot_%03d.png", i),
+      width = 1200, height = 800)
+  plot(e, x)
+  title(main = sprintf("%d samples", i))
+  lines(x, theta, 'l', lwd=3, col='red')
+  dev.off()
+}
